35e1f2ff40017eb933ce2dac034da37201fcc372
Fcorreia159 <correia481@gmail.com>
2023-09-19 13:59:55 -0300

    primeira parte do projeto

---
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja.modelo;
+
+import br.com.alura.loja.dao.CategoriaDao;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria(){
+
+    }
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
